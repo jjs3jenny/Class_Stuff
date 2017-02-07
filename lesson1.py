5491029fc75d831b1bd6ec883ba845a78e4be767
jjs3jenny <jjs3@cornell.edu>
2017-02-07 16:00:33 -0500

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,20 @@
+twitter = "@workisfun"
+print "twitter"
+print twitter
+print "My governor's name is Martin O'Mally"
+print "Contact Info:\n Shannon \t shannon@hearmecode.com"
+print "Lesson \t Topic"
+print "1 \t\t Strings and Conditionals"
+print "2 \t\t Lists and Loops"
+print "3 \t\t Dictionaries & File"
+print len(twitter)
+name = "Jenny"
+print len(name)
+print name[0:5]
+print name [:5]
+print name [0:]
+print name [1:]
+phone = "(202) 456-7890"
+print phone[0:9]
+print phone[1:4]
+print phone[6:9]
