7aca5327bd9241960a06ef020c015cb9aa0b17bd
jjs3jenny <jjs3@cornell.edu>
2017-02-07 16:00:50 -0500

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,8 @@
+name = "Shannon"
+age = 29
+print "My name is: {0} and my age is: {1}" .format(name, age)
+phone = "202-555-9876"
+print "Area Code: {0}".format(phone[0:3])
+print "Local: {0}".format (phone[4:])
+print "Different format: {0}".format(phone)
+print "Area Code: {0}\nLocal: {1}\nDifferent format: {2}" .format(phone[0:3],phone[4:],phone([0:3]))
